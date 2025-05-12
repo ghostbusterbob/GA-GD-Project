c77a67c9fbfd1516d2eb2f74bfaefcd907c82a56
Jaysen <jaysendaemon@gmail.com>
2025-05-12 11:59:28 +0200

    PickupUpdate

--- Assets/_Scripts/PlayerScripts/PickUp.cs
+++ Assets/_Scripts/PlayerScripts/PickUp.cs
@@ -1,21 +1,21 @@
+using System.Collections;
 using System.Data;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    string tag;
     [SerializeField] GameObject[] inventoryObjects;
     [SerializeField] Camera camera;
 
     [SerializeField] GameObject PickupDropLocation;
-
     public LayerMask layerMask;
-
     private GameObject heldObject;
+
     private GameObject objectInHand;
-
-
+    private bool lerping = false;
+    private bool addedForce = false;
+    private Rigidbody rb;
     private bool pickup = false;
 
     void Start()
@@ -25,61 +25,80 @@ public class PickUp : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && pickup == false)
+        if (Input.GetKeyDown(KeyCode.E) && !pickup)
         {
             Pickup();
             pickup = true;
         }
-        else if (Input.GetKeyDown(KeyCode.E) && pickup == true)
+        else if (Input.GetKeyDown(KeyCode.E) && pickup)
         {
             Drop();
         }
 
-        
-        
-    }
-
-    void Pickup()
-    {
-
-        RaycastHit hit;
-        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, 10f))
+        if (lerping)
         {
-            string hitName = hit.transform.name;
+            heldObject.transform.position = Vector3.Lerp(heldObject.transform.position, objectInHand.transform.position, 20f * Time.deltaTime);
+            heldObject.transform.rotation = Quaternion.Lerp(heldObject.transform.rotation, objectInHand.transform.rotation, 10f * Time.deltaTime);
+            heldObject.transform.localScale = Vector3.Lerp(heldObject.transform.localScale, objectInHand.transform.localScale, 10f * Time.deltaTime);
 
-        foreach (GameObject inventory in inventoryObjects)
-        {
-            if (inventory.name == hitName)
+            float dist = Vector3.Distance(heldObject.transform.position, objectInHand.transform.position);
+
+            if (dist < .01f)
             {
-                inventory.SetActive(true);
+                heldObject.SetActive(false);
+                objectInHand.SetActive(true);
+            }
+        }
+        else if (!lerping && rb != null)
+        {
+            rb.isKinematic = false;
+
+            if(!addedForce)
+            rb.AddForce(transform.forward * 200f);
+            addedForce = true;
+
+        }
+
+        void Pickup()
+        {
+            RaycastHit hit;
+            LayerMask layerMask = LayerMask.GetMask("Pickup");
+            if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, 5f, layerMask))
+            {
+                string hitName = hit.transform.name;
+                foreach (GameObject inventory in inventoryObjects)
+                {
+                    if (inventory.name == hitName)
+                    {
+                        objectInHand = inventory;
+                    }
+                }
+
+                Debug.Log("Picked up: " + hitName);
+                heldObject = hit.transform.gameObject;
+                lerping = true;
+                rb = hit.transform.gameObject.GetComponent<Rigidbody>();
+                rb.isKinematic = true;
+            
             }
         }
 
-        Debug.Log("Picked up: " + hitName);
-
-        heldObject = hit.transform.gameObject;
-        heldObject.SetActive(false); // Hide it in world
-        heldObject.transform.position = camera.transform.position;
-
-            
-
-
-
-        }
-    }
-
-
-    void Drop()
-    {
-        if (heldObject != null)
+        void Drop()
         {
-            objectInHand.transform.gameObject.SetActive(false);
-            heldObject.transform.position = camera.transform.position + camera.transform.forward * 2f;
-            heldObject.SetActive(true);
-            Debug.Log("Dropped: " + heldObject.name);
-            heldObject = null;
-            pickup = false;
+            if (heldObject != null)
+            {
+                lerping = false;
+                objectInHand.SetActive(false);
+                heldObject.SetActive(true);
+
+                Debug.Log("Dropped: " + heldObject.name);
+
+                heldObject = null;
+                pickup = false;
+                addedForce= false;
+
+
+            }
         }
     }
-
 }
