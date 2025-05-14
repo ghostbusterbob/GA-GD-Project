5f6b84933d4bec595ee49852570cd454d350b7e0
Jaysen <jaysendaemon@gmail.com>
2025-05-14 09:13:42 +0200

    UpdateWeaponLogic

--- Assets/_Scripts/PlayerScripts/PickUp.cs
+++ Assets/_Scripts/PlayerScripts/PickUp.cs
@@ -13,22 +13,17 @@ public class PickUp : MonoBehaviour
     private GameObject heldObject;
 
     private GameObject objectInHand;
+    private bool pickedUpWeapon = false;
     private bool lerping = false;
+    private bool handLerping = false;
     private bool addedForce = false;
     private Rigidbody rb;
     private bool pickup = false;
-
-    void Start()
-    {
-
-    }
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && !pickup)
         {
             Pickup();
-            pickup = true;
         }
         else if (Input.GetKeyDown(KeyCode.E) && pickup)
         {
@@ -43,7 +38,7 @@ public class PickUp : MonoBehaviour
 
             float dist = Vector3.Distance(heldObject.transform.position, objectInHand.transform.position);
 
-            if (dist < .01f)
+            if (dist < .03f)
             {
                 heldObject.SetActive(false);
                 objectInHand.SetActive(true);
@@ -53,17 +48,23 @@ public class PickUp : MonoBehaviour
         {
             rb.isKinematic = false;
 
-            if(!addedForce)
-            rb.AddForce(transform.forward * 200f);
+            if (!addedForce)
+                rb.AddForce(transform.forward * 200f);
             addedForce = true;
 
+        }
+
+        if (handLerping)
+        {
+            objectInHand.transform.position = Vector3.Lerp(objectInHand.transform.position, PickupDropLocation.transform.position, 30f * Time.deltaTime);
+            objectInHand.transform.rotation = Quaternion.Lerp(objectInHand.transform.rotation, camera.transform.rotation, 10f * Time.deltaTime);
         }
 
         void Pickup()
         {
             RaycastHit hit;
             LayerMask layerMask = LayerMask.GetMask("Pickup");
-            if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, 5f, layerMask))
+            if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, 3f, layerMask))
             {
                 string hitName = hit.transform.name;
                 foreach (GameObject inventory in inventoryObjects)
@@ -71,15 +72,22 @@ public class PickUp : MonoBehaviour
                     if (inventory.name == hitName)
                     {
                         objectInHand = inventory;
+
+                        if(objectInHand.tag == "weapon")
+                        {
+                            pickedUpWeapon = true;
+                        }
                     }
                 }
 
                 Debug.Log("Picked up: " + hitName);
                 heldObject = hit.transform.gameObject;
+                handLerping = true;
                 lerping = true;
                 rb = hit.transform.gameObject.GetComponent<Rigidbody>();
                 rb.isKinematic = true;
-            
+                pickup = true;
+
             }
         }
 
@@ -88,6 +96,7 @@ public class PickUp : MonoBehaviour
             if (heldObject != null)
             {
                 lerping = false;
+                handLerping = false;
                 objectInHand.SetActive(false);
                 heldObject.SetActive(true);
 
@@ -95,10 +104,12 @@ public class PickUp : MonoBehaviour
 
                 heldObject = null;
                 pickup = false;
-                addedForce= false;
+                addedForce = false;
 
-
+                pickup = false;
             }
         }
+
+        
     }
 }
