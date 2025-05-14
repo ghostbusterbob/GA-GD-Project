f45c13b9a72138defc6834501de2ee9962da1456
Yk_ <bo.bolweg@gmail.com>
2025-05-14 12:18:08 +0200

    Update

---
+++ Assets/_Scripts/PlayerScripts/Shooting.cs
@@ -0,0 +1,64 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.UI;
+
+public class Shooting : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private TMP_Text BulletUI;
+    public Camera camera;
+    public PickUp pickup;
+    private int bulletCount = 30;
+    private Component XPsystem;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log(bulletCount);
+        if (Input.GetKeyDown(KeyCode.Mouse0)&& pickup.pickedUpWeapon && bulletCount > 0)
+        {
+            weapon();
+            bulletCount -= 1;
+            updateBulletUI();
+        }
+        if (Input.GetKeyDown(KeyCode.R)) 
+        {
+            reload();
+        }
+    }
+    void weapon()
+    {
+        LayerMask layerMask = LayerMask.GetMask("Enemy");
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(camera.transform.position, camera.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            XPscript.instance.IncreaseXP(1);
+            Destroy(hit.transform.gameObject);
+
+
+        }
+
+    }
+    void reload()
+    {
+        
+            bulletCount = 30;
+            updateBulletUI() ;
+        
+    }
+
+    void updateBulletUI()
+    {
+        BulletUI.text = "30/" + bulletCount.ToString();
+    }
+
+}
